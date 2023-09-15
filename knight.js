1281c0f3f6b5df3f0985df5286c45c70b8791160
Michelle Appiah <michappiah67@gmail.com>
2023-09-15 20:46:33 +0000

    creating cordinates

--- knight.js
+++ knight.js
@@ -11,3 +11,55 @@ function createBoard() {
 
 const board = new Array(8).fill(0).map(() => new Array(8).fill(0));
 createBoard();
+
+function xSteps(x, move) {
+    if (move === 0) {
+        x = x + 1;
+    } else if (move === 1) {
+        x = x + 2;
+    } else if (move === 2) {
+        x = x + 2;
+    } else if (move === 3) {
+        x = x + 1;
+    } else if (move === 4) {
+        x = x - 1;
+    } else if (move === 5) {
+        x = x - 2;
+    } else if (move === 6) {
+        x = x - 2;
+    } else if (move === 7) {
+        x = x - 1;
+    }
+
+    if (x < 0 || x > 7) {
+        return -1;
+    } else {
+        return x; 
+    }
+}
+
+function ySteps(y, move) {
+    if (move === 0) {
+        y = y - 2;
+    } else if (move === 1) {
+        y = y - 1;
+    } else if (move === 2) {
+        y = y + 1;
+    } else if (move === 3) {
+        y = y + 2;
+    } else if (move === 4) {
+        y = y + 2;
+    } else if (move === 5) {
+        y = y + 1;
+    } else if (move === 6) {
+        y = y - 1;
+    } else if (move === 7) {
+        y = y - 2;
+    }
+
+    if (y < 0 || y > 7) {
+        return -1; 
+    } else {
+        return y; 
+    }
+}
